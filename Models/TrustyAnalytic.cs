5caa0e64c8749352bee3d2ac26874be588e4f717
Ryan Flores <rryanflorres@gmail.com>
2023-08-06 11:33:21 -0700

    Initial Commit

---
+++ Models/TrustyAnalytic.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class TrustyAnalytic
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public float OccurenceCount { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Models/TrustyAnalyticContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrustyAnalytics.Models;
+
+public class TrustyAnalyticContext : DbContext
+{
+    public TrustyAnalyticContext(DbContextOptions<TrustyAnalyticContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<TrustyAnalytic> Analytics { get; set; } = null!;
+}
